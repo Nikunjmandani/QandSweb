c64a67281f42549bdf2a6deed02918ed30977a5a
Nikunjmanda <nikunjmandani@gmail.com>
2018-06-30 11:03:11 +0530

    db cred

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var router = express.Router()
 
 app.use('/', routes)
 
-var dbURI = "mongodb://nikunj:nik9819@ds147390.mlab.com:47390/ivory";
+var dbURI = "mongodb://niku:nik9819@ds147390.mlab.com:47390/ivory";
 //put url db url
 mongoose.connect(dbURI, { useMongoClient: true });
 mongoose.Promise = global.Promise;
